4f9951b2d5218507b0799ddefa9667438d96e893
Pascal Hurni <phi@pragmaticpurist.org>
2023-07-29 19:22:24 +0200

    Fix (temporary) bug of DR list_files method returning empty array

--- lib/require_dir.rb
+++ lib/require_dir.rb
@@ -1,10 +1,14 @@
 # helper that requires all ruby file in a directory
 def require_dir(dirpath, options = {})
   Array(options[:first]).each {|name| require normalize_game_path(dirpath, name) }
-  $gtk.list_files(dirpath).each {|name| require normalize_game_path(dirpath, name) }
+  list_files(dirpath).each {|name| require normalize_game_path(dirpath, name) }
 end
 
 # DragonRuby specific helper functions used by `require_dir`
+def list_files(dir)
+  $gtk.exec("ls #{dir}").split("\n")
+end
+
 ABSOLUTE_GAME_PATH = File.expand_path($gtk.get_game_dir)
 def normalize_game_path(*paths)
   absolute_path = File.expand_path(File.join(*paths))
