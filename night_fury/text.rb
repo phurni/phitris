1ac0ccfe11759df2e93f13f74ffa2021733c6b70
Pascal Hurni <phi@pragmaticpurist.org>
2023-07-30 17:42:08 +0200

    Adapt dummy implementation of NF text

--- night_fury/text.rb
+++ night_fury/text.rb
@@ -1,4 +1,14 @@
 module NightFury
-  class Text < Object
+  class Text < GameObject
+    def initialize(label, options = {})
+      super(options)
+    end
+
+    def width
+      50
+    end
+    def height
+      20
+    end
   end
 end
