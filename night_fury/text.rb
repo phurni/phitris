04f40a8a9654984b8bd62f4c6cc92482d743c3c6
Pascal Hurni <phi@pragmaticpurist.org>
2023-07-31 18:11:37 +0200

    Implement real first version of NF Text

--- night_fury/text.rb
+++ night_fury/text.rb
@@ -1,14 +1,28 @@
 module NightFury
   class Text < GameObject
-    def initialize(label, options = {})
+    attr_accessor :x, :y
+    attr_reader :text, :width, :height, :color
+
+    def initialize(text, options = {})
       super(options)
+      self.text = text
+      self.x = options[:x] || 0
+      self.y = options[:y] || 0
+      self.color = options[:color] || NightFury::Color::WHITE
     end
 
-    def width
-      50
+    def text=(value)
+      @text = value
+      @width, @height = $gtk.calcstringbox(@text)
     end
-    def height
-      20
+
+    def color=(object)
+      @color = NightFury::Color.from(object)
+    end
+
+    def draw(args)
+      target_x, target_y, _, _ = to_draw_rect(x, y, 0, 0)
+      args.outputs.labels << [target_x, target_y, text, 0, 0, color.red, color.green, color.blue, color.alpha]
     end
   end
 end
